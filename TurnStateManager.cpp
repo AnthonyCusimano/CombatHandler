31d4c742cc16dc34e644c7059ad45c18c13be12f
Anthony Cusimano <anthonycusimano91@hotmail.com>
2019-08-31 15:14:14 -0400

    Update TurnStateManager.cpp

--- TurnStateManager.cpp
+++ TurnStateManager.cpp
@@ -9,7 +9,17 @@ TurnStateManager::TurnStateManager() {
 
 void TurnStateManager::EnemyArmyUpkeepEffects(BattleChapter* _theChapter)const {
 
+	for (int i = 0; i < _theChapter->myArmy.size(); ++i) {
+		
+		//errors because getItem returns a const Item
+		_theChapter->myArmy[i]->myItem->ProcessUpkeepAffect();
+		_theChapter->myArmy[i]->myWeapon->ProcessUpkeepAffect();
 
+		//skills
+
+		//tiles
+
+	}
 
 }
 
