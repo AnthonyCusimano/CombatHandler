367258a085b455360c61c86daec77dfc0727139c
Anthony Cusimano <anthonycusimano91@hotmail.com>
2019-08-30 22:49:31 -0400

    Add files via upload
    began implementation of EnemyArmyUpkeepEffects

---
+++ TurnStateManager.cpp
@@ -0,0 +1,21 @@
+#include "TurnStateManager.h"
+
+
+TurnStateManager::TurnStateManager() {
+
+	//:D
+
+}
+
+void TurnStateManager::EnemyArmyUpkeepEffects(BattleChapter* _theChapter)const {
+
+
+
+}
+
+
+TurnStateManager::~TurnStateManager() {
+
+	//:D
+
+}
