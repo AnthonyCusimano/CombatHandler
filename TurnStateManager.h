249dd4401b3db12d7eb2ba5dd107f29febcec780
Anthony Cusimano <anthonycusimano91@hotmail.com>
2019-10-01 22:51:58 -0400

    Update TurnStateManager.h
    comment pass

--- TurnStateManager.h
+++ TurnStateManager.h
@@ -5,43 +5,50 @@
 #include "BattleChapter.h"
 
 /**
-
+takes care of automated actions that take place between (at the beginning of) each phase
+of the player's turn and the AI's turn
 */
 class TurnStateManager {
 public:
 
 	/**
-	
+	no action taken
 	*/
 	TurnStateManager();
 
 	/**
-	
+	each character in the enemy army's roster will be run in this method
+	includes inventories, and later skills and squares
 	*/
 	void EnemyArmyUpkeepEffects(BattleChapter *_theChapter)const;
 
 	/**
-	
+	each character in the player's roster will be run in this method
+	includes inventories, and later skills and squares
 	*/
 	void AllyArmyUpkeepEffects(Player *_thePlayer)const;
 
 	/**
-	
+	each character in the enemy army's roster will be run in this method
+	includes inventories, and later skills and squares
 	*/
 	void EnemyArmyEndStepEffects(BattleChapter* _theChapter)const;
 
 	/**
-	
+	each character in the player's roster will be run in this method
+	includes inventories, and later skills and squares
 	*/
 	void AllyArmyEndStepEffects(Player* _thePlayer)const;
 
 	/**
-	
+	TRIHARD _square as pointer
+	processes the upkeep effects of a square
+	used inside methods within this class
 	*/
 	void ProcessSquareUpkeepEffects(GridSquare const _square, GenericCharacter* const _occupant)const;
 
 	/**
-	
+	no action taken
 	*/
 	~TurnStateManager();
 
